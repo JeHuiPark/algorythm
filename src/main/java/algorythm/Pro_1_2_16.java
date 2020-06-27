8bb03e79918bdcf883d25219bcb9be23a1195633
JehuiPark <pjh2359@gmail.com>
2020-06-27 21:39:11 +0900

    문제작성

---
+++ src/main/java/algorythm/Pro_1_2_16.java
@@ -0,0 +1,23 @@
+package algorythm;
+
+/**
+ * 유리수에 대한 덧셈, 뺄셈, 곱셈, 나눗셈을 지원하는 불변 데이터 타입 Rational 을 구현하라.
+ *
+ * Rational(int numerator, int denominator)
+ * Rational plus(Rational b) 이 값과 b의 합
+ * Rational minus(Rational b) 이 값과 b의 차이
+ * Rational times(Rational b) 이 값과 b의 곱
+ * Rational divideBy(Rational b) 이 값을 b로 나눈 몫
+ * Rational equals(Rational that) 동치성
+ * String toString() 문자열 표
+ *
+ * 오버플로 발생에 대해서는 걱정하지 않아도 된다 (연습문제 1.2.17 참조)
+ * 대신 분자, 분모는 long 타입을 사용하도록 한다.
+ * 분자와 분모가 항상 약분된 상태로 유지되도록 유클리드 알고리즘을 적용한다.
+ *
+ * 마지막으로 모든 메서드에 대해서 테스트 클라이언트를 작성한다.
+ */
+class Pro_1_2_16 {
+
+
+}
