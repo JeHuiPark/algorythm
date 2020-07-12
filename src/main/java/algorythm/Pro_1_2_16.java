972e95a69c2783869d571853d05be4549d12817e
JehuiPark <pjh2359@gmail.com>
2020-07-12 20:44:13 +0900

    최대 공약수를 구하는 메소드를 작성하라

--- src/main/java/algorythm/Pro_1_2_16.java
+++ src/main/java/algorythm/Pro_1_2_16.java
@@ -82,6 +82,16 @@ class Pro_1_2_16 {
     public String toString() {
       throw new UnsupportedOperationException();
     }
+
+    /**
+     * a 와 b의 최대 공약수를 리턴한다.
+     *
+     * https://ko.wikipedia.org/wiki/%EC%9C%A0%ED%81%B4%EB%A6%AC%EB%93%9C_%ED%98%B8%EC%A0%9C%EB%B2%95
+     */
+    static long gcd(long a, long b) {
+      if (b == 0) return a;
+      return gcd(b, a%b);
+    }
   }
 
 }
