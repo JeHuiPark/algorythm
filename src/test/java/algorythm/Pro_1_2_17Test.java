6701b78d5d904b19571d00bf5abf734c415bd9c3
JehuiPark <pjh2359@gmail.com>
2020-07-18 19:02:18 +0900

    테스트 컨텍스트를 분리하라

--- src/test/java/algorythm/Pro_1_2_17Test.java
+++ src/test/java/algorythm/Pro_1_2_17Test.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import algorythm.Pro_1_2_16.Rational;
 import algorythm.Pro_1_2_17.StrongRational;
 import java.util.Random;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
@@ -15,38 +16,6 @@ import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("InnerClassMayBeStatic")
 class Pro_1_2_17Test {
-
-  @Test
-  @DisplayName("overflow 테스트")
-  void test1() {
-    var rationalA = new StrongRational(Integer.MAX_VALUE - 2, Integer.MAX_VALUE);
-    var rationalB = new StrongRational(Integer.MAX_VALUE - 2, Integer.MAX_VALUE - 1);
-
-    assertEquals(
-        1,
-        gcd(rationalA.getNumerator(), rationalB.getDenominator()),
-        "overflow 테스트를 위한 사전조건을 만족하지 못합니다.");
-
-    assertEquals(
-        1,
-        gcd(rationalA.getDenominator(), rationalB.getNumerator()),
-        "overflow 테스트를 위한 사전조건을 만족하지 못합니다.");
-
-    assertAll(
-        () -> assertThrows(
-            AssertionError.class,
-            () -> rationalA.plus(rationalB)),
-        () -> assertThrows(
-            AssertionError.class,
-            () -> rationalA.minus(rationalB)),
-        () -> assertThrows(
-            AssertionError.class,
-            () -> rationalA.times(rationalB)),
-        () -> assertThrows(
-            AssertionError.class,
-            () -> rationalA.divideBy(rationalB))
-    );
-  }
 
   @Nested
   @DisplayName("강건한 유리수의 계산 결과는")
@@ -84,6 +53,48 @@ class Pro_1_2_17Test {
         );
       }
     }
-  }
 
+    @Nested
+    @DisplayName("overflow 를 유발하는 값이 주어지면")
+    class Context_case2 {
+
+      StrongRational givenA;
+      StrongRational givenB;
+
+      @BeforeEach
+      void setup() {
+        givenA = new StrongRational(Integer.MAX_VALUE - 2, Integer.MAX_VALUE);
+        givenB = new StrongRational(Integer.MAX_VALUE - 2, Integer.MAX_VALUE - 1);
+
+        assertEquals(
+            1,
+            gcd(givenA.getNumerator(), givenB.getDenominator()),
+            "overflow 테스트를 위한 사전조건을 만족하지 못합니다.");
+
+        assertEquals(
+            1,
+            gcd(givenA.getDenominator(), givenB.getNumerator()),
+            "overflow 테스트를 위한 사전조건을 만족하지 못합니다.");
+      }
+
+      @Test
+      @DisplayName("Assertion 오류가 발생한다.")
+      void test2() {
+        assertAll(
+            () -> assertThrows(
+                AssertionError.class,
+                () -> givenA.plus(givenB)),
+            () -> assertThrows(
+                AssertionError.class,
+                () -> givenA.minus(givenB)),
+            () -> assertThrows(
+                AssertionError.class,
+                () -> givenA.times(givenB)),
+            () -> assertThrows(
+                AssertionError.class,
+                () -> givenA.divideBy(givenB))
+        );
+      }
+    }
+  }
 }
