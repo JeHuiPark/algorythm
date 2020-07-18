82e51732c0f97525abd869de9d7cd4b01e9b7c2a
JehuiPark <pjh2359@gmail.com>
2020-07-18 14:57:12 +0900

    테스트 컨텍스트를 분리하라

--- src/test/java/algorythm/Pro_1_2_16Test.java
+++ src/test/java/algorythm/Pro_1_2_16Test.java
@@ -5,30 +5,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Random;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 
-
+@SuppressWarnings("InnerClassMayBeStatic")
 class Pro_1_2_16Test {
 
-  @RepeatedTest(10)
-  @DisplayName("분자와 분모가 항상 약분된 상태를 유지해야 한다. ")
-  void test1() {
-    int given_numerator = new Random().nextInt(100) + 1;
-    int given_denominator = new Random().nextInt(100) + 1;
-    int given_times1 = new Random().nextInt(100) + 1;
-    int given_times2 = new Random().nextInt(100) + 1;
+  @Nested
+  @DisplayName("Rational 은")
+  class Describe_Rational {
 
-    var rational1 = new Pro_1_2_16.Rational(
-        given_numerator * given_times1,
-        given_denominator * given_times1);
+    @RepeatedTest(10)
+    @DisplayName("분자와 분모가 항상 약분된 상태를 유지해야 한다. ")
+    void test1() {
+      int given_numerator = new Random().nextInt(100) + 1;
+      int given_denominator = new Random().nextInt(100) + 1;
+      int given_times1 = new Random().nextInt(100) + 1;
+      int given_times2 = new Random().nextInt(100) + 1;
 
-    var rational2 = new Pro_1_2_16.Rational(
-        given_numerator * given_times2,
-        given_denominator * given_times2);
+      var rational1 = new Pro_1_2_16.Rational(
+          given_numerator * given_times1,
+          given_denominator * given_times1);
 
-    assertAll(
-        () -> assertEquals(rational1.getNumerator(), rational2.getNumerator()),
-        () -> assertEquals(rational1.getDenominator(), rational2.getDenominator())
-    );
+      var rational2 = new Pro_1_2_16.Rational(
+          given_numerator * given_times2,
+          given_denominator * given_times2);
+
+      assertAll(
+          () -> assertEquals(rational1.getNumerator(), rational2.getNumerator()),
+          () -> assertEquals(rational1.getDenominator(), rational2.getDenominator())
+      );
+    }
   }
+
 }
